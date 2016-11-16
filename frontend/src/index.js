53ca39896dd074a931a615f788b89faa40106b91
kcubero <kcubero@softuresolutions.com>
2016-11-16 20:29:32 +0100

    frontend: añadida pagina y componente de tiendas

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -11,7 +11,7 @@ import 'material-design-icons';
 import {home} from './home/home';
 import {accesos} from './accesos/accesos';
 // import {socios} from './socios/socios';
-// import {tiendas} from './tiendas/tiendas';
+import {tiendas} from './tiendas/tiendas';
 // import {empresa} from './empresa/empresa';
 
 import routesConfig from './routes';
@@ -25,7 +25,7 @@ angular
   .module(app, ['ui.router', 'ngMaterial']) /* Declaracion de modulos inyectados */
   .config(routesConfig) /* Enrutamiento */
   .component('home', home) /* Componente angular del recurso home */
-  .component('accesos', accesos); /* Componente angular del recurso accesos */
+  .component('accesos', accesos) /* Componente angular del recurso accesos */
   // .component('socios', socios) /* Componente angular del recurso socios */
-  // .component('tiendas', tiendas) /* Componente angular del recurso tiendas */
+  .component('tiendas', tiendas); /* Componente angular del recurso tiendas */
   // .component('empresa', empresa); /* Componente angular del recurso empresa */
---
+++ frontend/src/tiendas/tiendas.js
@@ -0,0 +1,73 @@
+/* Clase del controlador del componente accesos */
+class TiendasController {
+
+  /* ngIngect */
+  constructor($http, $log, $state, $mdDialog, $document) {
+    /* Declaracion de variables y servicios */
+    this.$http = $http;
+    this.$log = $log;
+    this.$state = $state;
+    this.$mdDialog = $mdDialog;
+    this.$document = $document;
+    this.icon = './images/icon.png';
+    this.isRegistro = false;
+    this.isSocio = false;
+    this.isTienda = false;
+  }
+
+  registrar(tipo) {
+    this.isRegistro = true;
+    if (tipo === 0) {
+      this.isSocio = true;
+      this.isTienda = false;
+    }
+    if (tipo === 1) {
+      this.isTienda = true;
+      this.isSocio = false;
+    }
+  }
+
+  /* Funcion que se encarga de realizar el post de tarjetas */
+  registrarTarjeta() {
+    /* Ejecucion de post */
+    this.$http.post('http://localhost:8000/api/tarjetas', {nombre: this.nombre, primerApellido: this.primerApellido,
+      segundoApellido: this.segundoApellido, direccion: this.direccion, telefono: this.telefono, email: this.email})
+      .then(res => {
+        /* Caso de exito */
+        this.$log.debug('Respuesta del backend', res);
+        this.$mdDialog.show(
+          this.$mdDialog.alert()
+            .parent(angular.element(this.$document.body))
+            .clickOutsideToClose(true)
+            .title('Registro')
+            .textContent('¡Se ha registrado correctamente!')
+            .ok('Listo')
+        );
+      })
+      .catch(reason => {
+        /* Caso de fallo */
+        this.$log.debug('Fallo del backend', reason);
+      });
+  }
+
+  /* Funcion que se encarga de realizar el post de tarjetas */
+  registrarTienda() {
+    /* Ejecucion de post */
+    this.$http.post('http://localhost:8000/api/tiendas', {nombreTienda: this.nombreTienda, direccion: this.direccionTienda,
+      telefono: this.telefonoTienda})
+      .then(res => {
+        /* Caso de exito */
+        this.$log.debug('Respuesta del backend', res);
+      })
+      .catch(reason => {
+        /* Caso de fallo */
+        this.$log.debug('Fallo del backend', reason);
+      });
+  }
+}
+
+/* Exportacion de componente */
+export const tiendas = {
+  template: require('./tiendas.html'),
+  controller: TiendasController
+};
